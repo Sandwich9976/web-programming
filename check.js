a463d5346831f9184601e4365b3c96fe55417c44
Sandwich9976 <100012659+Sandwich9976@users.noreply.github.com>
2022-05-11 01:41:06 +0300

    Update check.js

--- check.js
+++ check.js
@@ -1,41 +1,31 @@
 const registerbutton = document.querySelector("#checker");
 registerbutton.addEventListener("click", RegisterCheck);
 
-function RegisterCheck(){
-	const email_TX = document.querySelector("#email");
-	const pass_TX = document.querySelector("#password");
-	const rpt_pass_TX = document.querySelector("#psw-repeat");
-	const email = email_TX.value;
-	const pass = pass_TX.value;
-	const rpt_pass = rpt_pass_TX.value;
-	
-	if(email.length<4 && !email_TX.classList.contains("red"))
-	{
-		email_TX.classList.add("red");
-	}
-	else if (email.length>=4 && email_TX.classList.contains("red"))
-	{
-		email_TX.classList.remove("red");
-	}
-	
-	if(pass.length<4 && !pass_TX.classList.contains("red"))
-	{
-		pass_TX.classList.add("red");
-	}
-	else if (pass.length>=4 && pass_TX.classList.contains("red"))
-	{
-		pass_TX.classList.remove("red");
-	}
-	
-	if(rpt_pass!=pass && !rpt_pass_TX.classList.contains("red"))
-	{
-		rpt_pass_TX.classList.add("red");
-	}
-	else if (rpt_pass==pass && rpt_pass_TX.classList.contains("red"))
-	{
-		rpt_pass_TX.classList.remove("red");
-	}
-};
-setInterval(RegisterCheck,1000)
+function RegisterCheck() {
+  const email_TX = document.querySelector("#email");
+  const pass_TX = document.querySelector("#password");
+  const rpt_pass_TX = document.querySelector("#psw-repeat");
+  const email = email_TX.value;
+  const pass = pass_TX.value;
+  const rpt_pass = rpt_pass_TX.value;
 
+  if (email.length < 4 && !email_TX.classList.contains("red")) {
+    email_TX.classList.add("red");
+  } else if (email.length >= 4 && email_TX.classList.contains("red")) {
+    email_TX.classList.remove("red");
+  }
+
+  if (pass.length < 4 && !pass_TX.classList.contains("red")) {
+    pass_TX.classList.add("red");
+  } else if (pass.length >= 4 && pass_TX.classList.contains("red")) {
+    pass_TX.classList.remove("red");
+  }
+
+  if (rpt_pass != pass && !rpt_pass_TX.classList.contains("red")) {
+    rpt_pass_TX.classList.add("red");
+  } else if (rpt_pass == pass && rpt_pass_TX.classList.contains("red")) {
+    rpt_pass_TX.classList.remove("red");
+  }
+}
+setInterval(RegisterCheck, 1000);
 
