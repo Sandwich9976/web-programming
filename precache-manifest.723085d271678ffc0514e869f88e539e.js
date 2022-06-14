85223f2f8466ea1acc869b5ad7344843f80a9e75
Sandwich9976 <petebato122@gmail.com>
2022-06-14 15:16:25 +0300

    Updates

---
+++ precache-manifest.723085d271678ffc0514e869f88e539e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87aae009cfe55c89f7ab",
+    "url": "/web-programming/static/css/main.995b15ae.chunk.css"
+  },
+  {
+    "revision": "87aae009cfe55c89f7ab",
+    "url": "/web-programming/static/js/main.23c7ba20.chunk.js"
+  },
+  {
+    "revision": "f5e9df8f453fd7e8630d",
+    "url": "/web-programming/static/js/runtime~main.d355a832.js"
+  },
+  {
+    "revision": "2bfa2e1ca284a22112ef",
+    "url": "/web-programming/static/js/2.8cca7415.chunk.js"
+  },
+  {
+    "revision": "00330752ade665012574",
+    "url": "/web-programming/static/js/3.9881a070.chunk.js"
+  },
+  {
+    "revision": "76cded546cf75f0d71d91710285d63d8",
+    "url": "/web-programming/index.html"
+  }
+];
